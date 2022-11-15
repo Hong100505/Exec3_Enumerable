bc1d75baaa96a333b6c31d7a70af9d1fc94a058d
ispan <ispan@DESKTOP-PAKSETB>
2022-11-15 15:52:59 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var number = Enumerable.Range(1, 10);
+			foreach (var item in number)
+			{
+				Console.WriteLine(item);
+			}
+
+			number = Enumerable.Range(10, 6);
+			foreach (var item in number)
+			{
+				Console.Write(item+" ");
+			}
+		}
+	}
+}
+ 
